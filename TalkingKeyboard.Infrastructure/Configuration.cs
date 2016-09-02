e23bd7f2df30c6587f9011a0db6f7c9bd9f8f2eb
Nan de R <A01221624@itesm.mx>
2016-09-01 19:21:47 -0500

    Configure Gaze-Selection for medium-fast mode
    Only changes configuration (specifically gaze timespans) to make it
    easier, yet fast, to select using gaze.

--- TalkingKeyboard.Infrastructure/Configuration.cs
+++ TalkingKeyboard.Infrastructure/Configuration.cs
@@ -5,9 +5,9 @@ namespace TalkingKeyboard.Infrastructure
     public static class Configuration
     {
         public static TimeSpan InitialRequiredGazeTime => TimeSpan.FromMilliseconds(500);
-        public static TimeSpan GazeTimeSpanBeforeAnimationBegins => TimeSpan.FromMilliseconds(50);
-        public static TimeSpan GazeKeepAliveTimeSpan => TimeSpan.FromMilliseconds(100);
-        private static TimeSpan GazeCoolDownTimeSpan => TimeSpan.FromMilliseconds(50);
+        public static TimeSpan GazeTimeSpanBeforeAnimationBegins => TimeSpan.FromMilliseconds(100);
+        public static TimeSpan GazeKeepAliveTimeSpan => TimeSpan.FromMilliseconds(70);
+        private static TimeSpan GazeCoolDownTimeSpan => TimeSpan.FromMilliseconds(200);
 
         public static TimeSpan GazeTimeSpanBeforeSelectionOccurs
             => InitialRequiredGazeTime + GazeTimeSpanBeforeAnimationBegins;
