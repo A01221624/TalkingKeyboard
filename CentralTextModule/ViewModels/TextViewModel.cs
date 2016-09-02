785eef2e84e5e96ed2dbb25ea3260945de6df1e9
Nan de R <A01221624@itesm.mx>
2016-09-01 19:31:09 -0500

    Create Command for deleting the last character

--- CentralTextModule/ViewModels/TextViewModel.cs
+++ CentralTextModule/ViewModels/TextViewModel.cs
@@ -12,11 +12,16 @@ namespace TalkingKeyboard.Modules.CentralTextModule.ViewModels
         public TextViewModel()
         {
             _addTextCommand = new DelegateCommand<string>(s => CurrentText += s);
+            _removeLastCharacterCommand =
+                new DelegateCommand(() => CurrentText = CurrentText.Remove(CurrentText.Length - 1), () => CurrentText.Length > 0)
+                    .ObservesProperty(() => CurrentText);
             Infrastructure.Commands.SetTextCommand.RegisterCommand(_addTextCommand);
+            Infrastructure.Commands.RemoveLastCharacterCommand.RegisterCommand(_removeLastCharacterCommand);
         }
 
-	    private string _currentText;
+	    private string _currentText = "";
 	    private ICommand _addTextCommand;
+	    private ICommand _removeLastCharacterCommand;
 
 	    public string CurrentText
 	    {
@@ -25,5 +30,11 @@ namespace TalkingKeyboard.Modules.CentralTextModule.ViewModels
 	    }
 
 	    public ICommand AddTextCommand => _addTextCommand;
+
+	    public ICommand RemoveLastCharacterCommand
+	    {
+	        get { return _removeLastCharacterCommand; }
+	        set { _removeLastCharacterCommand = value; }
+	    }
 	}
 }
