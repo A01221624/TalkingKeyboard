221c9c1c74612eca6acab140fdc4e38b6ffafa54
Nan de R <A01221624@itesm.mx>
2016-09-01 19:32:50 -0500

    Add QWERTY spanish keyboard, and necessary things
    Necessary things: Markup extension for selecting right character when
    shift is down (e.g. lowercase/uppercase) and command for toggling shift.
    
    This Keyboard does not handle accents, even though it includes them.

---
+++ TalkingKeyboard.Infrastructure/MarkupExtensions/SwitchBindingExtension.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace TalkingKeyboard.Infrastructure.MarkupExtensions
+{
+    /// <summary>
+    /// Markup extension which binds two possible strings and selects one based on a boolean.
+    /// Author: Thomas Levesque.
+    /// </summary>
+    /// <remarks>
+    /// Useful for Keyboards which change state based on other keys. Such as Shift ? Uppercase : Lowercase.
+    /// More information on: https://stackoverflow.com/questions/841808/wpf-display-a-bool-value-as-yes-no
+    /// </remarks>
+    public class SwitchBindingExtension : Binding
+    {
+        public SwitchBindingExtension()
+        {
+            Initialize();
+        }
+
+        public SwitchBindingExtension(string path)
+            : base(path)
+        {
+            Initialize();
+        }
+
+        public SwitchBindingExtension(string path, object valueIfTrue, object valueIfFalse)
+            : base(path)
+        {
+            Initialize();
+            this.ValueIfTrue = valueIfTrue;
+            this.ValueIfFalse = valueIfFalse;
+        }
+
+        public SwitchBindingExtension(RelativeSource relativeSource, string path, object valueIfTrue, object valueIfFalse)
+            : base(path)
+        {
+            Initialize();
+            this.RelativeSource = relativeSource;
+            this.ValueIfTrue = valueIfTrue;
+            this.ValueIfFalse = valueIfFalse;
+        }
+
+        private void Initialize()
+        {
+            this.ValueIfTrue = Binding.DoNothing;
+            this.ValueIfFalse = Binding.DoNothing;
+            this.Converter = new SwitchConverter(this);
+        }
+
+        [ConstructorArgument("valueIfTrue")]
+        public object ValueIfTrue { get; set; }
+
+        [ConstructorArgument("valueIfFalse")]
+        public object ValueIfFalse { get; set; }
+
+        private class SwitchConverter : IValueConverter
+        {
+            public SwitchConverter(SwitchBindingExtension switchExtension)
+            {
+                _switch = switchExtension;
+            }
+
+            private SwitchBindingExtension _switch;
+
+            #region IValueConverter Members
+
+            public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+            {
+                try
+                {
+                    bool b = System.Convert.ToBoolean(value);
+                    return b ? _switch.ValueIfTrue : _switch.ValueIfFalse;
+                }
+                catch
+                {
+                    return DependencyProperty.UnsetValue;
+                }
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+            {
+                return Binding.DoNothing;
+            }
+
+            #endregion
+        }
+    }
+}
---
+++ TalkingKeyboard.SingleKeyBoard/ViewModels/QwertySpanishSingleKeyboardViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using TalkingKeyboard.Infrastructure.Annotations;
+
+namespace TalkingKeyboard.Modules.SingleKeyBoard.ViewModels
+{
+    public class QwertySpanishSingleKeyboardViewModel : INotifyPropertyChanged
+    {
+        public QwertySpanishSingleKeyboardViewModel()
+        {
+            _setShiftDownCommand = new DelegateCommand<bool?>(b => IsShiftDown = b ?? false);
+            _toggleShiftDownCommand = new DelegateCommand(() => IsShiftDown = !IsShiftDown);
+            Infrastructure.Commands.ToggleShiftDownCommand.RegisterCommand(_toggleShiftDownCommand);
+        }
+
+        private bool _isShiftDown = false;
+        private ICommand _setShiftDownCommand;
+        private ICommand _toggleShiftDownCommand;
+
+        public bool IsShiftDown
+        {
+            get { return _isShiftDown; }
+            set
+            {
+                if (value == _isShiftDown) return;
+                _isShiftDown = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand SetShiftDownCommand
+        {
+            get { return _setShiftDownCommand; }
+            set { _setShiftDownCommand = value; }
+        }
+
+        public ICommand ToggleShiftDownCommand
+        {
+            get { return _toggleShiftDownCommand; }
+            set { _toggleShiftDownCommand = value; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
