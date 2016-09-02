cd397facef40f8e168817ca4df5482f56bc30744
Nan de R <A01221624@itesm.mx>
2016-09-01 19:36:08 -0500

    Register ShiftToggle and DeleteLastCharacter cmds
    This allows these commands (the names in git commit title are not the
    actual names, check them in diff) to be used throughout the modules.
    
    Consideration: ShiftToggle will probably have to be updated depending on
    the current Keyboard.

--- TalkingKeyboard.Infrastructure/Commands.cs
+++ TalkingKeyboard.Infrastructure/Commands.cs
@@ -5,5 +5,8 @@ namespace TalkingKeyboard.Infrastructure
     public static class Commands
     {
         public static CompositeCommand SetTextCommand = new CompositeCommand();
+        public static CompositeCommand SetShiftDownCommand = new CompositeCommand();
+        public static CompositeCommand ToggleShiftDownCommand = new CompositeCommand();
+        public static CompositeCommand RemoveLastCharacterCommand = new CompositeCommand();
     }
 }
