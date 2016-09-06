e2b56945c8eda284bc4369c5c3831c26e10b8892
Nan de R <A01221624@itesm.mx>
2016-09-06 17:16:15 -0500

    Add accent support (currently for spanish)

--- CentralTextModule/ViewModels/TextViewModel.cs
+++ CentralTextModule/ViewModels/TextViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Input;
 using Prism.Commands;
 using Prism.Events;
@@ -13,21 +14,14 @@ namespace TalkingKeyboard.Modules.CentralTextModule.ViewModels
     public class TextViewModel : BindableBase, ITextModel
     {
         private readonly IEventAggregator _eventAggregator;
+        private string _accentBuffer = string.Empty;
 
-        private string _currentText = "";
+        private string _currentText = string.Empty;
 
         public TextViewModel(IEventAggregator eventAggregator)
         {
             _eventAggregator = eventAggregator;
-            AddTextCommand = new DelegateCommand<string>(s =>
-            {
-                if (s.Length == 0) return;
-                if (CurrentText.Length > 0
-                    && CharacterClasses.PreceededByNonwhitespaceFollowedByWhitespace.Contains(s[0])
-                    && CharacterClasses.Whitespace.Contains(CurrentText[CurrentText.Length - 1]))
-                    CurrentText = CurrentText.Remove(CurrentText.Length - 1);
-                CurrentText += s;
-            });
+            AddTextCommand = new DelegateCommand<string>(AddText);
             RemoveLastCharacterCommand =
                 new DelegateCommand(() => CurrentText = CurrentText.Remove(CurrentText.Length - 1),
                     () => CurrentText.Length > 0)
@@ -53,6 +47,42 @@ namespace TalkingKeyboard.Modules.CentralTextModule.ViewModels
                 SetProperty(ref _currentText, value);
                 _eventAggregator.GetEvent<TextUpdatedEvent>().Publish();
             }
+        }
+
+        private void AddText(string s)
+        {
+            if (s.Length == 0) return;
+            var newIsAccent = CharacterClasses.Accents.Contains(s[0]);
+            if (!newIsAccent)
+            {
+                s = CombineWithAccentBuffer(s);
+                RemoveWhitespaceIfNecessaryBasedOn(s);
+                CurrentText += s;
+                _accentBuffer = string.Empty;
+            }
+            else
+            {
+                _accentBuffer = s;
+            }
+        }
+
+        private void RemoveWhitespaceIfNecessaryBasedOn(string s)
+        {
+            if (CurrentText.Length > 0 && CharacterClasses.PreceededByNonwhitespaceFollowedByWhitespace.Contains(s[0]) &&
+                CharacterClasses.Whitespace.Contains(CurrentText[CurrentText.Length - 1]))
+                CurrentText = CurrentText.Remove(CurrentText.Length - 1);
+        }
+
+        private string CombineWithAccentBuffer(string s)
+        {
+            Dictionary<char, char> possibleAccentuations;
+            char accentedCharacter;
+            if (_accentBuffer.Length > 0 && CharacterClasses.AccentLookup.TryGetValue(s[0], out possibleAccentuations) &&
+                possibleAccentuations.TryGetValue(_accentBuffer[0], out accentedCharacter))
+            {
+                s = accentedCharacter.ToString();
+            }
+            return s;
         }
     }
 }
--- TalkingKeyboard.Infrastructure/Constants/Constants.cs
+++ TalkingKeyboard.Infrastructure/Constants/Constants.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace TalkingKeyboard.Infrastructure.Constants
@@ -28,11 +29,25 @@ namespace TalkingKeyboard.Infrastructure.Constants
         public static char[] Whitespace = {' ', '\r', '\n', '\t'};
         public static char[] ColonsAndCommas = {'.', ';', ',', ':'};
         public static char[] ClosingCharacters = {'?', '!', ')', ']', '}', '>'};
+        public static char[] Accents = { '´', '¨'};
 
         public static char[] PreceededByNonwhitespaceFollowedByWhitespace =
             ColonsAndCommas.ToList().Concat(ClosingCharacters.ToList()).ToArray();
 
         public static char[] WordSeparators =
             Whitespace.ToList().Concat(PreceededByNonwhitespaceFollowedByWhitespace.ToList()).ToArray();
+        public static Dictionary<char, Dictionary<char, char>> AccentLookup = new Dictionary<char, Dictionary<char, char>>()
+        {
+            { 'a', new Dictionary<char, char>() { { '´', 'á'} } },
+            { 'e', new Dictionary<char, char>() { { '´', 'é'} } },
+            { 'i', new Dictionary<char, char>() { { '´', 'í'} } },
+            { 'o', new Dictionary<char, char>() { { '´', 'ó'} } },
+            { 'u', new Dictionary<char, char>() { { '´', 'ú'}, { '¨', 'ü' } } },
+            { 'A', new Dictionary<char, char>() { { '´', 'Á'} } },
+            { 'E', new Dictionary<char, char>() { { '´', 'É'} } },
+            { 'I', new Dictionary<char, char>() { { '´', 'Í'} } },
+            { 'O', new Dictionary<char, char>() { { '´', 'Ó'} } },
+            { 'U', new Dictionary<char, char>() { { '´', 'Ú'}, { '¨', 'Ü' } } }
+        };
     }
 }
